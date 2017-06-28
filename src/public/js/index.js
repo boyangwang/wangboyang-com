1e4ceaa889a40c5fa25371605831ac0a7c6912bc
Boyang <wangboyang1991@gmail.com>
2017-06-28 09:45:37 +0800

    remove 2 buttons and leave only works+about

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -15,40 +15,39 @@ function main() {
   setupMobileHoverEffectDelay();
 
   setupScrollButtons();
-
 }
 
 function setPageDivHeightToBeWindowHeight() {
-    var screenHeight = $(window).height();
-    $(".first-page-div").css('min-height', toPxString(screenHeight));
-    // var footerDivHeight = $('.footer-texts-div').height() + $('.copyright-footer-div').height() + 100;
-    // var remainingHeight = $('.third-page-div').height() - $('.self-container-div').height();
-    // footerDivHeight = remainingHeight > footerDivHeight ? remainingHeight : footerDivHeight;
-   // 	$(".footer-div").css("height", toPxString(footerDivHeight));
-   	// alert('resized!');
+  var screenHeight = $(window).height();
+  $(".first-page-div").css('min-height', toPxString(screenHeight));
+  // var footerDivHeight = $('.footer-texts-div').height() + $('.copyright-footer-div').height() + 100;
+  // var remainingHeight = $('.third-page-div').height() - $('.self-container-div').height();
+  // footerDivHeight = remainingHeight > footerDivHeight ? remainingHeight : footerDivHeight;
+  // 	$(".footer-div").css("height", toPxString(footerDivHeight));
+  // alert('resized!');
 }
 
 function bindPolyfill() {
   if (!Function.prototype.bind) {
-    Function.prototype.bind = function(oThis) {
+    Function.prototype.bind = function (oThis) {
       if (typeof this !== 'function') {
         // closest thing possible to the ECMAScript 5
         // internal IsCallable function
         throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
       }
 
-      var aArgs   = Array.prototype.slice.call(arguments, 1),
-          fToBind = this,
-          fNOP    = function() {},
-          fBound  = function() {
-            return fToBind.apply(this instanceof fNOP
-                   ? this
-                   : oThis,
-                   // here arguments different from above. because it's referenced from current scoping,
-                   // it uses the one from fBound. That is, when fBound is called, the arguments passed.
-                   // Conclusion: later invokation args come at end, bind passed arguments at beginning
-                   aArgs.concat(Array.prototype.slice.call(arguments)));
-          };
+      var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP ?
+            this :
+            oThis,
+            // here arguments different from above. because it's referenced from current scoping,
+            // it uses the one from fBound. That is, when fBound is called, the arguments passed.
+            // Conclusion: later invokation args come at end, bind passed arguments at beginning
+            aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
 
       if (this.prototype) {
         // Function.prototype doesn't have a prototype property
@@ -62,61 +61,61 @@ function bindPolyfill() {
 }
 
 function toPxString(height) {
-	return parseInt(height) + 'px';
+  return parseInt(height) + 'px';
 }
 
 function scrollToElem(elem) {
-	$('html, body').animate({
-		scrollTop: elem.offset().top - parseInt(elem.css('margin-top'))
-	}, 700);
+  $('html, body').animate({
+    scrollTop: elem.offset().top - parseInt(elem.css('margin-top'))
+  }, 700);
 }
 
 function fadeOutAndRemovePreloaders() {
   $('.inner-loader-div').add(preloaderImg).fadeOut(800);
   $('.outer-loader-div').delay(800).fadeOut(300,
-    function() {
+    function () {
       $('.outer-loader-div').remove();
     }
   );
 }
 
-function whichTransitionEvent(){
-    var t;
-    var el = document.createElement('fakeelement');
-    var transitions = {
-      'transition':'transitionend',
-      'OTransition':'oTransitionEnd',
-      'MozTransition':'transitionend',
-      'WebkitTransition':'webkitTransitionEnd'
-    }
+function whichTransitionEvent() {
+  var t;
+  var el = document.createElement('fakeelement');
+  var transitions = {
+    'transition': 'transitionend',
+    'OTransition': 'oTransitionEnd',
+    'MozTransition': 'transitionend',
+    'WebkitTransition': 'webkitTransitionEnd'
+  }
 
-    for(t in transitions){
-        if( el.style[t] !== undefined ){
-            return transitions[t];
-        }
+  for (t in transitions) {
+    if (el.style[t] !== undefined) {
+      return transitions[t];
     }
+  }
 }
 
 function setupScrollButtons() {
-  $('.navbar-item-a.projects').on('click', function(e) {
+  $('.main-container-item-a.works').on('click', function (e) {
     // prevent standard hash navigation (avoid blinking in IE)
     e.preventDefault();
     scrollToElem($('.second-page-div'));
   });
 
-  $('.navbar-item-a.about').on('click', function(e) {
+  $('.main-container-item-a.about').on('click', function (e) {
     // prevent standard hash navigation (avoid blinking in IE)
     e.preventDefault();
     scrollToElem($('.third-page-div'));
   });
 
-  $('.downward-arrow-div').on('click', function(e) {
+  $('.downward-arrow-div').on('click', function (e) {
     // prevent standard hash navigation (avoid blinking in IE)
     e.preventDefault();
     scrollToElem($('.second-page-div'));
   });
 
-  $('.upward-arrow-div').on('click', function(e) {
+  $('.upward-arrow-div').on('click', function (e) {
     // prevent standard hash navigation (avoid blinking in IE)
     e.preventDefault();
     scrollToElem($('.first-page-div'));
@@ -126,21 +125,23 @@ function setupScrollButtons() {
 function setupPreloader() {
   if (preloaderImg[0].complete) {
     preloaderImg.fadeIn(50);
-  }
-  else {
-    preloaderImg.load(function() { $(this).fadeIn(300); });
+  } else {
+    preloaderImg.load(function () {
+      $(this).fadeIn(300);
+    });
   }
 }
 
 function setupLogoImgToggle() {
-  $('.logo-img').on('mouseenter click', function(e) {
+  $('.logo-img').on('mouseenter click', function (e) {
     $('.youve-entered-p').toggle();
   });
 }
 
 function isTouchDevice() {
-  return 'ontouchstart' in window        // works on most browsers
-      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+  return 'ontouchstart' in window // works on most browsers
+    ||
+    navigator.maxTouchPoints; // works on IE10/11 and Surface
 };
 
 function hoverEffectElemClickHandler(e) {
@@ -149,7 +150,7 @@ function hoverEffectElemClickHandler(e) {
     return false;
   }
   elem.attr('data-onTheFly', true);
-  setTimeout(function() {
+  setTimeout(function () {
     elem.removeAttr('data-onTheFly');
     window.location = elem.attr('href');
   }, 450);
